b5be4a32b85afa2b7b1c454d26aac3d614685480
Kushal Mohta <km578@duke.edu>
2024-06-27 12:54:08 +0530

    base query

--- src/functions/buildRestaurants.js
+++ src/functions/buildRestaurants.js
@@ -4,8 +4,22 @@ const pool = require('../utils/db');
 exports.buildMenus = async () => {
     try {
         const client = await pool.connect();
-        // TODO: Implement query
+
+        const query = `
+        SELECT json_agg(
+            json_build_object(
+                'id', r.restaurant_id,
+                'name', r.name
+                // TODO: Add more restaurant details
+            )
+        )
+        FROM Restaurants r;
+        `;
+
+        const result = await client.query(query);
         client.release();
+
+        return result.rows[0].json_agg;
     } catch (error) {
         console.error('Error executing query:', error);
         throw new Error('Failed to retrieve menu items');
