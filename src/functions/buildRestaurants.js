787b5e003a83d15dee41c91d3d4291e6abb975bd
Kushal Mohta <km578@duke.edu>
2024-06-27 13:24:15 +0530

    client side queries

--- src/functions/buildRestaurants.js
+++ src/functions/buildRestaurants.js
@@ -143,8 +143,142 @@ exports.buildMenus = async () => {
 exports.buildClientMenus = async () => {
     try {
         const client = await pool.connect();
-        // TODO: Implement query
+
+        const query = `
+        SELECT json_agg(
+            json_build_object(
+                'id', r.restaurant_id,
+                'name', r.name,
+                'image', r.image,
+                'icon', r.icon, 
+                'cuisine', r.cuisine,
+                'description', r.description,
+                'indefinitelyClosed', r.indefinitelyClosed,
+                'email', r.email,
+                'phone', r.phone,
+                'minimumOrderAmmount', r.minimumOrderAmount,
+                'deliveryFee', r.deliveryFee,
+                'tags', (
+                    SELECT json_agg(t.name)
+                    FROM RestaurantTags rt
+                    JOIN Tags t ON rt.tag_id = t.tag_id
+                    WHERE rt.restaurant_id = r.restaurant_id
+                ),
+                'hours', json_build_object(
+                    'mondayOpen', r.mondayOpen,
+                    'mondayClose', r.mondayClose,
+                    'tuesdayOpen', r.tuesdayOpen,
+                    'tuesdayClose', r.tuesdayClose,
+                    'wednesdayOpen', r.wednesdayOpen,
+                    'wednesdayClose', r.wednesdayClose,
+                    'thursdayOpen', r.thursdayOpen,
+                    'thursdayClose', r.thursdayClose,
+                    'fridayOpen', r.fridayOpen,
+                    'fridayClose', r.fridayClose,
+                    'saturdayOpen', r.saturdayOpen,
+                    'saturdayClose', r.saturdayClose,
+                    'sundayOpen', r.sundayOpen,
+                    'sundayClose', r.sundayClose
+                ),
+                'featured', (
+                    SELECT json_agg(
+                        json_build_object(
+                            'id', i.item_id,
+                            'name', i.name,
+                            'description', i.description,
+                            'price', i.price,
+                            'image', i.image,
+                            'modifierGroups', (
+                                SELECT json_agg(
+                                    json_build_object(
+                                        'id', mg.modifier_group_id,
+                                        'name', mg.name,
+                                        'minNeeded', mg.min_needed,
+                                        'maxNeeded', mg.max_needed,
+                                        'required', true,
+                                        'modifiers', (
+                                            SELECT json_agg(
+                                                json_build_object(
+                                                    'id', mgi.item_id,
+                                                    'name', mi.name,
+                                                    'price', mgi.new_price
+                                                )
+                                            )
+                                            FROM ModifierGroupItems mgi
+                                            JOIN Items mi ON mgi.item_id = mi.item_id
+                                            WHERE mgi.modifier_group_id = mg.modifier_group_id
+                                        )
+                                    )
+                                )
+                                FROM ItemModifierGroups img
+                                JOIN ModifierGroups mg ON img.modifier_group_id = mg.modifier_group_id
+                                WHERE img.item_id = i.item_id
+                            )
+                        )
+                    )
+                    FROM Items i
+                    WHERE i.restaurant_id = r.restaurant_id AND i.is_featured = true
+                ),
+                'categories', (
+                    SELECT json_agg(
+                        json_build_object(
+                            'id', c.category_id,
+                            'name', c.name,
+                            'display_category', c.display_category,
+                            'image', 'http://picsum.photos/150/150', -- Static image, replace as needed
+                            'items', (
+                                SELECT json_agg(
+                                    json_build_object(
+                                        'id', i.item_id,
+                                        'name', i.name,
+                                        'description', i.description,
+                                        'price', i.price,
+                                        'image', i.image,
+                                        'modifierGroups', (
+                                            SELECT json_agg(
+                                                json_build_object(
+                                                    'id', mg.modifier_group_id,
+                                                    'name', mg.name,
+                                                    'minNeeded', mg.min_needed,
+                                                    'maxNeeded', mg.max_needed,
+                                                    'modifiers', (
+                                                        SELECT json_agg(
+                                                            json_build_object(
+                                                                'id', mgi.item_id,
+                                                                'name', mi.name,
+                                                                'price', mgi.new_price
+                                                            )
+                                                        )
+                                                        FROM ModifierGroupItems mgi
+                                                        JOIN Items mi ON mgi.item_id = mi.item_id
+                                                        WHERE mgi.modifier_group_id = mg.modifier_group_id
+                                                    )
+                                                )
+                                            )
+                                            FROM ItemModifierGroups img
+                                            JOIN ModifierGroups mg ON img.modifier_group_id = mg.modifier_group_id
+                                            WHERE img.item_id = i.item_id
+                                        )
+                                    )
+                                )
+                                FROM Items i
+                                WHERE i.category_id = c.category_id
+                            )
+                        )
+                    )
+                    FROM Categories c
+                    WHERE c.restaurant_id = r.restaurant_id
+                )
+            )
+        )
+        FROM Restaurants r;
+              
+        `;
+        const result = await client.query(query);
         client.release();
+
+        return result;
+
     } catch (error) {
         console.error('Error executing query:', error);
         throw new Error('Failed to retrieve menu items for client');
