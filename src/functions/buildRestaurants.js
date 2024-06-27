53d68366ae32c17c01e2056c61074220a6a6e156
Kushal Mohta <km578@duke.edu>
2024-06-27 13:00:08 +0530

    Added restaurant information to query

--- src/functions/buildRestaurants.js
+++ src/functions/buildRestaurants.js
@@ -9,8 +9,33 @@ exports.buildMenus = async () => {
         SELECT json_agg(
             json_build_object(
                 'id', r.restaurant_id,
-                'name', r.name
-                // TODO: Add more restaurant details
+                'name', r.name,
+                'image', r.image,
+                'cuisine', r.cuisine,
+                'description', r.description,
+                'indefinitelyClosed', r.indefinitelyClosed,
+                'email', r.email,
+                'phone', r.phone,
+                'icon', r.icon,
+                'minimumOrderAmount', r.minimumOrderAmount,
+                'deliveryFee', r.deliveryFee,
+                'hours', json_build_object(
+                    'mondayOpen', r.mondayOpen,
+                    'mondayClose', r.mondayClose,
+                    'tuesdayOpen', r.tuesdayOpen,
+                    'tuesdayClose', r.tuesdayClose,
+                    'wednesdayOpen', r.wednesdayOpen,
+                    'wednesdayClose', r.wednesdayClose,
+                    'thursdayOpen', r.thursdayOpen,
+                    'thursdayClose', r.thursdayClose,
+                    'fridayOpen', r.fridayOpen,
+                    'fridayClose', r.fridayClose,
+                    'saturdayOpen', r.saturdayOpen,
+                    'saturdayClose', r.saturdayClose,
+                    'sundayOpen', r.sundayOpen,
+                    'sundayClose', r.sundayClose
+                )
+                // TODO: Add categories and items
             )
         )
         FROM Restaurants r;
