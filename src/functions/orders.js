91c320e8bcd663f333feb2cc8ec1ea58e04ca519
Kushal Mohta <km578@duke.edu>
2024-07-10 17:15:04 +0530

    adds order and sends relevant emails

--- src/functions/orders.js
+++ src/functions/orders.js
@@ -113,6 +113,50 @@ exports.create = async (event) => {
             };
         }
     }
+
+    // send email
+ 
+    // get all restaurants in order and send each an email with their respective order
+    emailList.forEach(async (email) => {
+        console.log(email.order, email)
+        const splitEmails = email.email.split(',').map(e => e.trim());
+        const msg = {
+            to: [ `${email.customer}@duke.edu`, ...splitEmails],
+            from: {
+                email: 'info@campusenterprises.com',
+                name :email.order[0].restaurantName
+            },
+            templateId: 'd-98449f3672524fe2b1e65d9296650545',
+            dynamic_template_data: {
+                order: email.order,
+                customer: email.customer,
+                home_address: email.home_address,
+                subtotal: email.subtotal,
+                deliveryFee: email.delivery_fee,
+                total: email.total,
+                phone_number: email.phone,
+                paymentMethod: payWithCard ? `Card ending in ${last4}` : 'Food points at dropoff'
+            }
+        };
+        sgMail.send(msg);
+    }
+    )
+    
+    try {
+        console.log("Invoking addOrderToUser function with params:", params);
+        const response = await lambda.invoke(params).promise();
+        console.log("Lambda invocation response:", response);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Order created', response, paymentIntent })
+        };
+    } catch (error) {
+        console.error('Error invoking addOrderToUser Lambda function:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed to initiate order creation', details: error.message })
+        };
+    }
 };
 
 
